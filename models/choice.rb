56acfe67155757d52d0d187c59e18043c1cd601d
posseirigoyen <posseirigoyen@gmail.com>
2021-05-28 10:47:41 -0300

    Fixed one-to-one relation setup in models, ASK: how to create a test for one to one relations?

--- models/choice.rb
+++ models/choice.rb
@@ -1,6 +1,5 @@
 class Choice < Sequel::Model
-	#many_to_one:questions
-	one_to_one:questions
+	many_to_one:questions #in sequel, to setup a one to one relation, its needed to list it as many_to_one in the table that has the foreign key
 	#one_to_many:responses
 	one_to_many:outcomes
 	many_to_one:survey
