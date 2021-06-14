966166843fa802b8d7e20316326222a3532c5242
posseirigoyen <posseirigoyen@gmail.com>
2021-06-14 19:54:37 -0300

    Fixed typo in migration

--- db/migrations/000018_add_cascade_to_foreign_keys.rb
+++ db/migrations/000018_add_cascade_to_foreign_keys.rb
@@ -12,7 +12,7 @@ Sequel.migration do
     end
     alter_table(:questions) do
         drop_foreign_key [:choice_id], name: :questions_choice_id_fkey
-        add_foreign_key [:choice_id], :careers, on_delete: :cascade
+        add_foreign_key [:choice_id], :choices, on_delete: :cascade
     end
   end
 
@@ -26,6 +26,10 @@ Sequel.migration do
     alter_table(:surveys) do
       drop_foreign_key [:career_id], name: :surveys_career_id_fkey
       add_foreign_key [:career_id], :careers
+    end
+    alter_table(:questions) do
+        drop_foreign_key [:choice_id], name: :questions_choice_id_fkey
+        add_foreign_key [:choice_id], :choices
     end
   end
 end
