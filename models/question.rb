25508e0b24bc4175330a0d034b61f7a3691fdcd9
posseirigoyen <posseirigoyen@gmail.com>
2021-06-24 22:09:39 -0300

    Added first_two Question model method

--- models/question.rb
+++ models/question.rb
@@ -3,6 +3,18 @@ class Question < Sequel::Model
 	many_to_one:choices
 	#one_to_many:responses
 
+	def self.first_two
+      question=Question.first(2)
+      return false if question.length < 2
+      check=true
+      question.each do |q| 
+      	outcomes=Outcome.where(choice_id: q.choice_id)
+      	l=outcomes.count
+      	check = check && l>0
+      end
+      check
+   end 
+
 	def validate
 		super
 		errors.add(:description, 'cannot be empty') if !description || description.empty?
