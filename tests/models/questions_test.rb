54e6c2eedc574a5b51a39326bdb19653cb3fde23
gaxel11 <axel_maicco@hotmail.com>
2021-09-09 01:10:16 -0300

    Agrego test para metodo Question.first_to

--- tests/models/questions_test.rb
+++ tests/models/questions_test.rb
@@ -2,35 +2,57 @@ require File.expand_path '../../test_helper.rb', __FILE__
 
 class QuestionTest < MiniTest::Unit::TestCase
 MiniTest::Unit::TestCase
-	
-  def test_question_has_one_choice
-  
-    #Arrange
-    choice=Choice.create()
-		
-    #Act
-    question1=Question.create(description:'a',choice_id: choice.choice_id)
-    question2=Question.create(description:'b',choice_id: choice.choice_id)
-		
-    #Assert
-		
-    assert_equal (question1.choice_id == question2.choice_id), true
 
-  end
+  #= def test_question_has_one_choice
+
+    #Arrange
+#    choice=Choice.create()
+
+    #Act
+#    question1=Question.create(description:'a')
+#    question2=Question.create(description:'b')
+
+    #Assert
+
+#    assert_equal (question1.choice_id == question2.choice_id), true
+
+  #= end
 
   def test_question_description_is_not_null
     question = Question.new
-    
+
     question.description=nil
-    
+
     assert_equal question.valid?, false
   end
 
   def test_question_description_is_not_empty
     question = Question.new
-    
+
     question.description=''
-    
+
     assert_equal question.valid?, false
   end
+
+	def test_question_first_two_true
+		#Arrange
+		question1 = Question.create(description:'a')
+    question2 = Question.create(description:'b')
+		choice = Choice.create()
+		#Act
+		Outcome.create(choice_id: choice.choice_id)
+		Outcome.create(choice_id: choice.choice_id)
+		#Assert
+		assert_equal Question.first_two, false
+	end
+
+	def test_question_first_two_false
+		#Arrange
+		choice = Choice.create()
+		#Act
+		Outcome.create(choice_id: choice.choice_id)
+		Outcome.create(choice_id: choice.choice_id)
+		#Assert
+		assert_equal Question.first_two, false
+	end
 end
