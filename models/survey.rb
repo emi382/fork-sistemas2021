b992f45504e1123166ef1b548c238bc90453d4c9
gaxel11 <axel_maicco@hotmail.com>
2021-10-09 08:05:37 -0300

    [finishes #179859715] Implemento filterByDate

--- models/survey.rb
+++ models/survey.rb
@@ -1,8 +1,19 @@
 class Survey < Sequel::Model
 	many_to_one:careers
 	
-	def self.filterbyDate(startDate, finishDate)
+	def self.filterByDate(startDate, finishDate)
  		#TODO:make this function return all surveys within the specified dates
+		 surveyStruct = Struct.new(:career_id, :survey_id)
+		 surveys = Survey.all
+		 surveyArray = Array.new
+		 i = 0
+		 surveys.each do |survey|
+			if (survey.created_at >= DateTime.parse(startDate) && survey.created_at <= DateTime.parse(finishDate))
+				surveyArray[i] = surveyStruct.new(survey.career_id, survey.survey_id)
+				i=i+1
+			end
+		end
+		return surveyArray
 	end
 
 	#this function returns a structure of careers and an accumulator representing the survey count of said careers
